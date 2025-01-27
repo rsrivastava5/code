dfd643d3cb71db1aa31beb264e33bff9b8694b99
Rahul Srivastava <rahul.srivastava@gmail.com>
2025-01-26 20:25:21 -0800

    [cpp][cs2a] fourth quest final commit

--- cpp/cs2a/q4/Looping_Functions.cpp
+++ cpp/cs2a/q4/Looping_Functions.cpp
@@ -3,6 +3,7 @@
 // Looping_Functions.cpp
 //
 
+#include <cmath>
 #include <iostream>
 #include <sstream>
 
@@ -11,23 +12,21 @@ using namespace std;
 // Give the user 6 chaances to guess the secret number n (0-10). If they get it,
 // say so and rteurn true. Else saay so and return false.
 bool play_game(int n) {
-    bool rv = false;
-    int num_guesses = 0;
     string user_input;
     int guess;
 
-    cout << "Welcome to my number guessing game\n\n\n";
+    cout << "Welcome to my number guessing game\n\n";
     for (int i = 0; i < 6; ++i) {
         cout << "Enter your guess: ";
         getline(cin, user_input);
         istringstream(user_input) >> guess;
-        cout << "You entered: " << guess << endl;
+        cout << "\nYou entered: " << guess << endl;
         if (guess == n) {
             cout << "You found it in " << i + 1 << " guess(es)." << endl;
             return true;
         }
     }
-    cout << "\nI'm sorry. You didn't find my number.\n";
+    cout << "I'm sorry. You didn't find my number.\n";
     cout << "It was " << n << endl;
     return false;
 }
@@ -57,3 +56,55 @@ size_t count_chars(string s, char c) {
 
 // Use Euclid's algorithm to calculate the GCD of the given numbers
 size_t gcd(size_t a, size_t b) {
+    size_t smaller = (a < b ? a : b);
+    size_t larger = (a >= b ? a : b);
+    size_t r = larger % smaller;
+
+    while (r != 0) {
+        larger = smaller;
+        smaller = r;
+        r = larger % smaller;
+    }
+    return smaller;
+}
+
+// Return a string of the form n1,n2,n3,... for the given AP.
+string get_ap_terms(int a, int d, size_t n) {
+    string rv = "";
+    for (size_t i = 0; i < n; ++i) {
+        stringstream ss;
+        int term = a + i * d;
+        ss << term;
+        string temp = ss.str();
+        rv += temp;
+        if (i != (n-1))
+            rv += ",";
+    }
+    return rv;
+}
+
+// Return a string of the form n1,n2,n3,... for the given GP.
+string get_gp_terms(double a, double r, size_t n) {
+string rv = "";
+    for (size_t i = 0; i < n; ++i) {
+        stringstream ss;
+        double term = a * pow(r, i);
+        ss << term;
+        string temp = ss.str();
+        rv += temp;
+        if (i != (n-1))
+            rv += ",";
+    }
+    return rv;
+}
+
+double get_nth_fibonacci_number(size_t n) {
+    double term_1 = 1, term_2 = 1;
+    double rv = 1;
+    for (size_t i = 3; i <= n; ++i) {
+        term_2 = term_1;
+        term_1 = rv;
+        rv = term_1 + term_2;
+    }
+    return rv;
+}
--- cpp/cs2a/q4/quest4.cpp
+++ cpp/cs2a/q4/quest4.cpp
@@ -3,9 +3,14 @@
 
 int main(int argc, char **argv) {
     std::cout << "First quest: " << play_game(5) << std::endl;
-    std::cout << "Second quest: " << etox(2.1, 1) << std::endl << etox(3,2) << std::endl << etox(3,10) << std::endl;
+    std::cout << "Second quest: " << etox(1,10) << std::endl;
     std::cout << "Third quest: " << count_chars("srivastava", 'a') << std::endl;
     size_t n1 = 1701, n2 = 3768;
     std::cout << "Fourth quest: " << "GCD of " <<  n1 << " and " << n2 << " is " << gcd(n1, n2) << std::endl;
+    n1 = 15, n2 = 50;
+    std::cout << "Fourth quest: " << "GCD of " <<  n1 << " and " << n2 << " is " << gcd(n1, n2) << std::endl;
+    std::cout << "Fifth quest: " << "AP terms " << get_ap_terms(-5,-3,5) << std::endl;
+    std::cout << "Sixth quest: " << "GP Terms " << get_gp_terms(4, -0.5, 6) << std::endl;
+    std::cout << "Seventh quest: " << "Fibonacci " << get_nth_fibonacci_number(7) << std::endl;
     return 0;
 }
